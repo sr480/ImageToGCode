ba3fd4b4dfc3e181849ab5e570e2efaf429f07d1
sr480 <sr480@live.ru>
2015-10-19 16:47:43 +0300

    *

--- ImageToGCode/Engine/StepInterpolator.cs
+++ ImageToGCode/Engine/StepInterpolator.cs
@@ -11,17 +11,10 @@ namespace ImageToGCode.Engine
         public Pixel GetPixel(Image image, Vector position)
         {
             if (position.X > image.Width - 1 || position.Y > image.Height - 1 || position.X < 0 || position.Y < 0)
-                throw new Exception("Point is out of image dimension");
+                //throw new Exception("Point is out of image dimension");
+                return null;
 
-            List<Pixel> neighbors = new List<Pixel>();
-            neighbors.Add(image.GetPixel((int)position.X, (int)position.Y + 1));
-            neighbors.Add(image.GetPixel((int)position.X + 1, (int)position.Y + 1));
-            neighbors.Add(image.GetPixel((int)position.X + 1, (int)position.Y));
-            neighbors.Add(image.GetPixel((int)position.X, (int)position.Y));
-
-            Pixel closest = neighbors.OrderBy(p => (p - position).Length).First();
-
-            return new Pixel(closest.Intensity, position);
+            return new Pixel(image.GetPixel((int)Math.Round(position.X), (int)Math.Round(position.Y)).Intensity, position);
         }
     }
 }
